12111c96ea992a8cf46d338bf27e02606a12642f
spottieMF <cph-m227@cphbusiness.dk>
2018-11-09 10:53:34 +0100

    Start

---
+++ JavaApplication94/src/javaapplication94/JavaApplication94.java
@@ -0,0 +1,16 @@
+package javaapplication94;
+
+/**
+ *
+ * @author Morten
+ */
+public class JavaApplication94 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
